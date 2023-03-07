6da73d555b92ad90c3472f9ab1a2bb88e31f45a0
kevin-roan <kevinroan@proton.me>
2023-03-07 09:22:35 +0530

    blog helper

---
+++ helpers/blog-helper.js
@@ -0,0 +1,24 @@
+var db = require("../config/connection");
+var collection = require("../config/collections");
+var objectId = require("mongodb").ObjectId;
+
+module.exports = {
+  addBlog: (blog, callback) => {
+    db.get()
+      .collection("product")
+      .insertOne(product)
+      .then((data) => {
+        callback(data.insertedId);
+      });
+  },
+  getAllBlog: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db
+        .get()
+        .collection(collection.BLOG_COLLECTION)
+        .find()
+        .toArray();
+      resolve(blogs);
+    });
+  },
+};
